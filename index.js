132463cb4db6374fb062e25e733142b8ca541fa3
Chigozie Mbama Mykel <92220456+mbamamykel77@users.noreply.github.com>
2023-04-07 00:50:36 +0100

    code modification

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let toggleBtn = document.querySelector('.toggle')
 let body = document.querySelector('body')
-let media = document.querySelector('#media')
+// let media = document.querySelector('#media')
 // let grey1 = document.querySelector('.grey1')
 // let grey2 = document.querySelector('.grey2')
 // let cardDiv = document.querySelectorAll('.card')
